dafc997d719fad26d0a24a1bada7334d8b933c93
BonTeves43 <bonteves43@gmail.com>
2025-10-22 10:01:10 +0800

    fixing the prisma

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -2,8 +2,8 @@ import { SignIn } from "@stackframe/stack";
 import Link from "next/link";
 
 export default function SignInPage() {
-  return <div className="min-h-screen flex items-center justify-center bg:gray-100"> 
-    <div>
+  return <div className="min-h-screen flex items-center justify-center bg:gradient-to-br from-purple-50 to-purple-10"> 
+    <div className="max-w-md w-full space-y-8">
         <SignIn /> 
         <Link href="/"> Go Back Home </Link>
     </div>
---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+    import {PrismaClient} from "@prisma/client";
+
+    const prisma = new PrismaClient();
+
+    async function main() {   
+        const demoUserId = "cb46628e-fc69-4c53-a7c1-f3d3d34cfa56";
+
+        //create sample products 
+        await prisma.product.createMany({
+            data: Array.from({length: 25}).map((_, i) => ({
+            userId: demoUserId,
+            name: ` Product ${i + 1}`,
+            price: parseFloat((Math.random() * 90 + 10).toFixed(2)),
+            quantity: Math.floor(Math.random() * 20),
+            lowStockAt: 5,
+            createAt: new Date(Date.now() - 10000 * 60 * 60 * 24 * (i * 5)),
+            })),
+        });
+    }
+
+    main()
+        .catch((e) => {
+            console.error(e);
+            process.exit(1);
+        })
+        .finally(async () => {
+            await prisma.$disconnect();
+        }); 
